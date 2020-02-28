cabf265611c49cd90efbdf614814b65b55ef22d7
Mattie <55457022+serenetangerine@users.noreply.github.com>
2020-02-27 21:23:02 -0700

    Add files via upload
    Initial commit

---
+++ primes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from math import sqrt
+from datetime import datetime
+
+# initialize time to show elapsed time when quit
+startTime = datetime.now()
+
+# import primes.txt into primes[] list
+try:
+    file = open('primes.txt', 'r')
+    primes = file.readlines()
+    file.close()
+except:
+    # initialize primes.txt and primes[] list
+    primes = [2, 3]
+    file = open('primes.txt', 'a+')
+    for prime in primes:
+        file.write(str(prime) + '\n')
+    file.close()
+
+# main loop
+# initialize test number as last prime + 2
+n = int(primes[-1]) + 2
+count = 0
+try:
+    while True:
+        # check divisibility of n by all primes < sqrt(n)
+        for prime in primes:
+            prime = int(prime)
+            if prime <= sqrt(float(n)):
+                if n % prime == 0:
+                    # n is not prime
+                    n = n + 2
+                    break
+            else:
+                # n is prime because it is not divisible by any primes less than
+                # the square root of n (relatively easy to prove)
+                print(str(n))
+                primes.append(n)
+                with open('primes.txt', 'a') as file:
+                    file.write(str(n) + '\n')
+                n = n + 2
+                count = count + 1
+                break
+except KeyboardInterrupt:
+    elapsedTime = datetime.now() - startTime
+    print('\n\nScript ran for %s and found %s primes.' % (str(elapsedTime), str(count)))
